14aed265c655ae4b8e761e8536765f32c5da224d
Diyor-Khasanov <baxacap@gmail.com>
2025-12-05 16:18:52 +0500

    about section

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import About1 from "../assets/about1.png";
+import About2 from "../assets/about2.png";
+
+const About = () => {
+  return (
+    <div>
+      <p className="text-blue-600 text-center text-lg">
+        Here are our some of the best clients.
+      </p>
+      <h1 className="text-center text-5xl mb-4 font-semibold">
+        What People Say About Us
+      </h1>
+
+      <div className="flex justify-around my-15">
+        <div className="flex p-6 bg-gray-100 rounded-xl max-w-[600px] gap-4">
+          <img src={About1} alt="Category 1" />
+          <div>
+            <h1 className="text-4xl font-bold">Hamza Faizi</h1>
+            <p className="my-3 text-xl text-gray-600">
+              Don’t waste time, just order! This is the best website to puschase smart watches.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex p-6 bg-gray-100 rounded-xl max-w-[600px] gap-4">
+          <img src={About2} alt="Category 1" />
+          <div>
+            <h1 className="text-4xl font-bold">Hafiz Huzaifa</h1>
+            <p className="my-3 text-xl text-gray-600">
+              I’ve been purchasing smart watches of Mohid for a long time. All the products are good quality.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
